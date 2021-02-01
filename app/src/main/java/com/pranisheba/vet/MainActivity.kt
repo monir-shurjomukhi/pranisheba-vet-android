b02617623b18db52f4e763e79bba89b0e875b508
Monir <mknishad1@ymail.com>
2021-02-01 17:33:11 +0600

    add project files

---
+++ app/src/main/java/com/pranisheba/vet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pranisheba.vet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
