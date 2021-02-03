1577edf4882802cd212036e5fa53bccd9759abdd
Monir <mknishad1@ymail.com>
2021-02-03 18:27:09 +0600

    add sign up activity

---
+++ app/src/main/java/com/pranisheba/vet/ui/activity/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.pranisheba.vet.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pranisheba.vet.R
+
+class SignUpActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_up)
+  }
+}
