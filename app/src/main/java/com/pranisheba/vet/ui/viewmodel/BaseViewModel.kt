8d776bb03683a52c57d7bd237048da4c1698d4e7
Monir <monir@shurjomukhi.com.bd>
2021-02-11 17:55:49 +0600

    add sign up view model

--- app/src/main/java/com/pranisheba/vet/ui/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/pranisheba/vet/ui/viewmodel/BaseViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.pranisheba.vet.preference.VetPreference
 
-class BaseViewModel(application: Application) : AndroidViewModel(application) {
+open class BaseViewModel(application: Application) : AndroidViewModel(application) {
   val progress = MutableLiveData<Boolean>()
   val message = MutableLiveData<String>()
   val preference = VetPreference(application)
---
+++ app/src/main/java/com/pranisheba/vet/ui/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,6 @@
+package com.pranisheba.vet.ui.viewmodel
+
+import android.app.Application
+
+class SignUpViewModel(application: Application) : BaseViewModel(application) {
+}
