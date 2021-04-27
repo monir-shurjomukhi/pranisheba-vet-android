c476383c0e745c125393456f91c0774384ee94cb
Monir <mknishad1@ymail.com>
2021-04-27 20:55:18 +0600

    add grid items to home fragment

--- app/src/main/java/com/pranisheba/vet/ui/activity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,9 +21,9 @@ class HomeFragment : Fragment() {
     homeViewModel =
       ViewModelProvider(this).get(HomeViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_home, container, false)
-    val textView: TextView = root.findViewById(R.id.text_home)
+    //val textView: TextView = root.findViewById(R.id.text_home)
     homeViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
+      //textView.text = it
     })
     return root
   }
