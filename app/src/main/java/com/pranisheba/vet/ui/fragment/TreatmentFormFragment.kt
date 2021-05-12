36b66f1f359355eeab380f2fe0d371f9a7b6376a
Monir <mknishad1@ymail.com>
2021-05-12 14:07:29 +0600

    add treatment form activity

---
+++ app/src/main/java/com/pranisheba/vet/ui/fragment/TreatmentFormFragment.kt
@@ -0,0 +1,22 @@
+package com.pranisheba.vet.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pranisheba.vet.R
+
+
+class TreatmentFormFragment : Fragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_treatment_form, container, false)
+  }
+
+
+}
