32857d6c219d6bc778d702b4bbc2f426de5293c0
Monir <mknishad1@ymail.com>
2021-06-28 19:00:04 +0600

    launch language screen

--- app/src/main/java/com/pranisheba/vet/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/SplashActivity.kt
@@ -5,15 +5,32 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.pranisheba.vet.R
+import com.pranisheba.vet.preference.VetPreference
 
 class SplashActivity : AppCompatActivity() {
+
+  private lateinit var preference : VetPreference
+  private lateinit var launchIntent: Intent
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
 
+    preference = VetPreference(this)
+
+    launchIntent = if (preference.getSelectedLanguage().isNullOrBlank()) {
+      Intent(this, LanguageActivity::class.java)
+    } else {
+      Intent(this, MainActivity2::class.java)
+    }
+
     Handler().postDelayed(Runnable {
-      startActivity(Intent(this, MainActivity2::class.java))
+      startActivity(launchIntent)
       finish()
     }, 300)
+  }
+
+  companion object {
+    private const val TAG = "SplashActivity"
   }
 }
