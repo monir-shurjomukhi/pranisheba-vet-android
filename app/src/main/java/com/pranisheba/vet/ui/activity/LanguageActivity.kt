acb739474af19d8fbc086221f2e58df74677931d
Monir <mknishad1@ymail.com>
2021-06-28 19:16:13 +0600

    launch pranisheba vet website

--- app/src/main/java/com/pranisheba/vet/ui/activity/LanguageActivity.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/LanguageActivity.kt
@@ -1,12 +1,24 @@
 package com.pranisheba.vet.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pranisheba.vet.R
+
 
 class LanguageActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_language)
+  }
+
+  fun launchWebsite(view: View) {
+    val webpage: Uri = Uri.parse("https://www.pranishebavet.com.bd/")
+    val intent = Intent(Intent.ACTION_VIEW, webpage)
+    if (intent.resolveActivity(packageManager) != null) {
+      startActivity(intent)
+    }
   }
 }
