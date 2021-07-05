20b8d35519820ae5bdf1ed50ee58d0d558576b50
Monir <mknishad1@ymail.com>
2021-07-05 19:28:43 +0600

    add coordinator layout

--- app/src/main/java/com/pranisheba/vet/ui/activity/ServiceDetailsActivity.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/ServiceDetailsActivity.kt
@@ -1,25 +1,24 @@
 package com.pranisheba.vet.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.pranisheba.vet.R
 import kotlinx.android.synthetic.main.activity_farm_and_farmer_info.toolbar
-import kotlinx.android.synthetic.main.activity_service_related_info.*
+import kotlinx.android.synthetic.main.activity_service_details2.*
 
-class ServiceRelatedInfoActivity : AppCompatActivity() {
+class ServiceDetailsActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_service_related_info)
+    setContentView(R.layout.activity_service_details2)
+
+    toolbar.title = "Toolbar Title"
 
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
     supportActionBar?.setDisplayShowHomeEnabled(true)
 
-    treatmentServiceCard.setOnClickListener {
-      startActivity(Intent(this, ServiceDetailsActivity::class.java))
-    }
+    collapsing_toolbar_layout.title = "Collapsing Toolbar Title"
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
