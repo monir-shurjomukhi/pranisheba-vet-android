f61e239f9a91521307e969397d08e4a7f69d82f1
Monir <mknishad1@ymail.com>
2021-08-03 21:07:49 +0600

    update base view model

--- app/src/main/java/com/pranisheba/vet/ui/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/pranisheba/vet/ui/viewmodel/BaseViewModel.kt
@@ -4,20 +4,9 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.pranisheba.vet.preference.VetPreference
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 
 open class BaseViewModel(application: Application) : AndroidViewModel(application) {
   val progress = MutableLiveData<Boolean>()
   val message = MutableLiveData<String>()
   val preference = VetPreference(application)
-
-  private val viewModelJob = Job()
-  protected val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
-
-  override fun onCleared() {
-    super.onCleared()
-    viewModelJob.cancel()
-  }
 }
