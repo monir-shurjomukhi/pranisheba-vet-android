45f588e466f36a17dbfbe72b0b478cd8584775cd
Monir <mknishad1@ymail.com>
2021-09-05 18:55:17 +0600

    add agent registration activity

---
+++ app/src/main/java/com/pranisheba/vet/ui/activity/AgentRegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.pranisheba.vet.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pranisheba.vet.R
+
+class AgentRegistrationActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_agent_registration)
+  }
+}
