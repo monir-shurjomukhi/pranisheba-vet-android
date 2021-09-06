54107345fc6fd57fe2a47bbc713e73a4282178ae
Monir <mknishad1@ymail.com>
2021-09-06 16:32:06 +0600

    finish design agent registration activity

--- app/src/main/java/com/pranisheba/vet/ui/activity/AgentRegistrationActivity.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/AgentRegistrationActivity.kt
@@ -1,12 +1,29 @@
 package com.pranisheba.vet.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.pranisheba.vet.R
+import com.pranisheba.vet.databinding.ActivityAgentRegistrationBinding
 
 class AgentRegistrationActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityAgentRegistrationBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_agent_registration)
+    binding = ActivityAgentRegistrationBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    setSupportActionBar(binding.toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.setDisplayShowHomeEnabled(true)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // handle arrow click here
+    if (item.itemId == android.R.id.home) {
+      onBackPressed()
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
