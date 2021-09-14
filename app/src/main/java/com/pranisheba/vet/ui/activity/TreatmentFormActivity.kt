d4f2329f389215990fef220bf972c905656ff886
Monir <mknishad1@ymail.com>
2021-09-14 19:07:25 +0600

    add validation for owner info

--- app/src/main/java/com/pranisheba/vet/ui/activity/TreatmentFormActivity.kt
+++ app/src/main/java/com/pranisheba/vet/ui/activity/TreatmentFormActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.aceinteract.android.stepper.StepperNavListener
 import com.pranisheba.vet.R
 import com.pranisheba.vet.databinding.ActivityTreatmentFormBinding
+import com.pranisheba.vet.ui.fragment.AnimalInfoFragment
+import com.pranisheba.vet.ui.fragment.DiseaseInfoFragment
+import com.pranisheba.vet.ui.fragment.OwnerInfoFragment
+
 
 class TreatmentFormActivity : AppCompatActivity(), StepperNavListener {
 
   private lateinit var binding: ActivityTreatmentFormBinding
+
+  private var name: String = ""
+  private var mobile: String = ""
+  private var email: String = ""
+  private var farmName: String = ""
+  private var address: String = ""
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -27,7 +38,52 @@ class TreatmentFormActivity : AppCompatActivity(), StepperNavListener {
     binding.stepper.stepperNavListener = this
 
     binding.buttonPrevious.setOnClickListener { findNavController(R.id.frame_stepper).navigateUp() }
-    binding.buttonNext.setOnClickListener { binding.stepper.goToNextStep() }
+    binding.buttonNext.setOnClickListener {
+
+      val navHostFragment: Fragment? = supportFragmentManager.findFragmentById(R.id.frame_stepper)
+      val foregroundFragment = if (navHostFragment == null) null else navHostFragment.childFragmentManager
+        .fragments[0]
+
+      when (foregroundFragment) {
+        is OwnerInfoFragment -> {
+          name = foregroundFragment.getNameLayout().editText?.text.toString()
+          mobile = foregroundFragment.getMobileLayout().editText?.text.toString()
+          email = foregroundFragment.getEmailLayout().editText?.text.toString()
+          farmName = foregroundFragment.getFarmNameLayout().editText?.text.toString()
+          address = foregroundFragment.getAddressLayout().editText?.text.toString()
+
+          validateOwnerInfo(foregroundFragment)
+        }
+
+        is AnimalInfoFragment -> {
+
+        }
+
+        is DiseaseInfoFragment -> {
+
+        }
+      }
+    }
+  }
+
+  private fun validateOwnerInfo(fragment: OwnerInfoFragment) {
+    when {
+      name.isEmpty() -> {
+        fragment.getNameLayout().editText?.error = getString(R.string.name_required)
+        return
+      }
+      mobile.isEmpty() -> {
+        fragment.getMobileLayout().editText?.error = getString(R.string.mobile_number_required)
+        return
+      }
+      address.isEmpty() -> {
+        fragment.getAddressLayout().editText?.error = getString(R.string.address_required)
+        return
+      }
+      else -> {
+        binding.stepper.goToNextStep()
+      }
+    }
   }
 
   override fun onStepChanged(step: Int) {
@@ -36,9 +92,9 @@ class TreatmentFormActivity : AppCompatActivity(), StepperNavListener {
     binding.buttonPrevious.isVisible = step != 0
 
     if (step == 2) {
-      binding.buttonNext.setImageResource(R.drawable.ic_check)
+      binding.buttonNext.setImageResource(com.pranisheba.vet.R.drawable.ic_check)
     } else {
-      binding.buttonNext.setImageResource(R.drawable.ic_right)
+      binding.buttonNext.setImageResource(com.pranisheba.vet.R.drawable.ic_right)
     }
   }
 
@@ -57,8 +113,14 @@ class TreatmentFormActivity : AppCompatActivity(), StepperNavListener {
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     // handle arrow click here
     if (item.itemId == android.R.id.home) {
-      onBackPressed()
+      finish()
     }
     return super.onOptionsItemSelected(item)
+  }
+
+  private fun getForegroundFragment(): Fragment? {
+    val navHostFragment: Fragment? = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+    return if (navHostFragment == null) null else navHostFragment.childFragmentManager
+      .fragments[0]
   }
 }
--- app/src/main/java/com/pranisheba/vet/ui/fragment/OwnerInfoFragment.kt
+++ app/src/main/java/com/pranisheba/vet/ui/fragment/OwnerInfoFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
 import com.pranisheba.vet.databinding.FragmentOwnerInfoBinding
 
 
@@ -18,5 +19,25 @@ class OwnerInfoFragment : Fragment() {
   ): View {
     binding = FragmentOwnerInfoBinding.inflate(inflater, container, false)
     return binding.root
+  }
+
+  fun getNameLayout() : TextInputLayout {
+    return binding.nameLayout
+  }
+
+  fun getMobileLayout() : TextInputLayout {
+    return binding.mobileLayout
+  }
+
+  fun getEmailLayout() : TextInputLayout {
+    return binding.emailLayout
+  }
+
+  fun getFarmNameLayout() : TextInputLayout {
+    return binding.farmNameLayout
+  }
+
+  fun getAddressLayout() : TextInputLayout {
+    return binding.addressLayout
   }
 }
