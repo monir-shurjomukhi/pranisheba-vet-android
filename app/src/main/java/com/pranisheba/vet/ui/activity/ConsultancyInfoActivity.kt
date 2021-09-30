4dd2e38013d71b3ef2eab6e652e7f9c6bf3c5752
mknishad <mknishad1@ymail.com>
2021-09-30 18:59:59 +0600

    create consultancy info activity

---
+++ app/src/main/java/com/pranisheba/vet/ui/activity/ConsultancyInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.pranisheba.vet.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pranisheba.vet.R
+
+class ConsultancyInfoActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_consultancy_info)
+  }
+}
